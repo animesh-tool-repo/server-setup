96679ad207f80c6d90378409fd56d7a1e764ee63
Animesh Kumar Singh <animesh.workplace@gmail.com>
2022-08-18 12:00:59 +0530

    updated creation of default environment

--- setup.sh
+++ setup.sh
@@ -43,8 +43,8 @@ fi
 if [[ ! -f "$CURR_DIR/logs/default_env" ]]; then
 	center "${GREEN}Setting up default environment${NORMAL}"
 	. $MODULE_PREFIX/environment_modules/init/bash
-	module load mamba
-	mamba create -y -f $CURR_DIR/envs/default.yaml
+	module load mamba \ 
+		&& mamba create -y -f $CURR_DIR/envs/default.yaml
 	touch $CURR_DIR/logs/default_env
 fi
 
