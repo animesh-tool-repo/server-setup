bf96453291a64f325c15e0d409516ee5198883ab
Animesh Kumar Singh <animesh.workplace@gmail.com>
2022-08-19 13:59:46 +0530

    updated the mamba command and setting up the environment modules folder as planned

--- setup.sh
+++ setup.sh
@@ -29,9 +29,8 @@ fi
 
 if [[ ! -f "$CURR_DIR/logs/env_module" ]]; then
 	center "${GREEN}Setting up Environment Module${NORMAL}"
-		${MODULE_PREFIX}/bin/micromamba create -y -c conda-forge -r ${MODULE_PREFIX} -n environment_modules environment-modules 
-		mv ${MODULE_PREFIX}/envs/environment_modules ${MODULE_PREFIX}
-		rm -rf ${MODULE_PREFIX}/envs ${MODULE_PREFIX}/pkgs
+		${MODULE_PREFIX}/bin/micromamba create -y -c conda-forge -r ${MODULE_PREFIX}/condaenv \
+			-p ${MODULE_PREFIX}/environment_modules environment-modules 
 		rm -rf $MODULE_PREFIX/environment_modules/modulefiles
 		cp -r $CURR_DIR/modules $MODULE_PREFIX/
 		echo "module use  $MODULE_PREFIX/modules" >> $MODULE_PREFIX/environment_modules/init/modulerc
